a408c15e3ed47b3b7638bf19fd970e334f547ae6
Arnaud Lachaume <arnaud.lachaume@maestrano.com>
2016-08-18 21:58:23 +1000

    add nginx - add default env vars

--- 2.3/docker-entrypoint.sh
+++ 2.3/docker-entrypoint.sh
@@ -4,6 +4,10 @@ set -e
 # Go to app directory
 cd /app
 
+# Set default environment variables
+export RACK_ENV=${RACK_ENV:-production}
+export RAILS_ENV=${RAILS_ENV:-production}
+
 # Clone app from git
 if [ -n "$GIT_URL" ] && [ -n "$GIT_BRANCH" ]; then
   [ -d /app/.git ] || git clone --branch "$GIT_BRANCH" --depth 50 $GIT_URL /app
@@ -17,6 +21,12 @@ if [ -n "$GIT_URL" ] && [ -n "$GIT_BRANCH" ]; then
 
   # Pre-compile assets
   [ "$NO_COMPILE" == "true" ] || bundle exec rake assets:precompile
+fi
+
+# Nginx configuration
+if [ ! "$NO_NGINX" == "true" ] && [ -f /app/nginx.conf ]; then
+  rm -f /etc/nginx/sites-enabled/*
+  cp -p /app/nginx.conf /etc/nginx/sites-enabled/
 fi
 
 # Perform command - default is "foreman start" (see Dockerfile)
