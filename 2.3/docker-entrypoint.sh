cccf1b0c5587235c751bd934f310d1133f57e049
Arnaud Lachaume <arnaud.lachaume@maestrano.com>
2017-01-10 16:51:58 +1100

    add default foreman opts

--- 2.3/docker-entrypoint.sh
+++ 2.3/docker-entrypoint.sh
@@ -5,6 +5,7 @@ set -e
 cd /app
 
 # Set default environment variables
+export FOREMAN_OPTS=${FOREMAN_OPTS:-""}
 export RACK_ENV=${RACK_ENV:-production}
 export RAILS_ENV=${RAILS_ENV:-production}
 export RAILS_LOG_TO_STDOUT=${RAILS_LOG_TO_STDOUT:-true}
